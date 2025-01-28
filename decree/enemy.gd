f1ce608e1c739c7de217c807b34696e446c44f3a
Sam-Goldie <samuelmgoldie@gmail.com>
2025-01-28 15:04:13 -0500

    fixed movement bug

--- decree/enemy.gd
+++ decree/enemy.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 func move(player_pos):
 	var distance = self.position - player_pos
-	if abs(distance[0]) + abs(distance[1]) == 1:
+	if abs(distance[0]) + abs(distance[1]) == 16:
 		return
-	elif abs(distance[0]) < abs(distance[1]):
+	elif distance[1] == 0 or abs(distance[0]) < abs(distance[1]):
 		if distance[0] < 0:
 			self.position += Vector2(16,0)
 		else:
